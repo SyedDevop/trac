37b97328e2f72c3ccfcf756fa13086b89013c41f
SyedDevop <syeds.devops007@gmail.com>
2026-09-12 15:25:29 +0530

    feat(test): add test cmd for zig build

--- build.zig
+++ build.zig
@@ -32,4 +32,15 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| run_cmd.addArgs(args);
+
+    const test_step = b.step("test", "Run unit tests");
+    const exe_unit_tests = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    test_step.dependOn(&run_exe_unit_tests.step);
 }
